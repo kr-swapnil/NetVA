33512da6fd10a70c3d2bd90f17f94d7743423181
kr-swapnil <swapnil.kr@yahoo.com>
2023-03-24 22:06:27 +0530

    updated

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' A protein-protein interactions dataset of breast cancer
+#'
+#' 
+#' An igraph graph object containing 143751 edges (interactions) among 10597 nodes (proteins).
+#'
+#' 
+#' @name bca_ppi
+#' @docType data
+#' @title A protein-protein interactions dataset of breast cancer from CancerNet database.
+#' 
+#'
+#' @keywords datasets
+#' @references Meng X, Wang J, Yuan C. et al. CancerNet: a database for decoding multilevel molecular interactions across diverse cancer types. Oncogenesis 4, e177 (2015).
+NULL
+
